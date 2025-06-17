132a9f582e64535746144babb9782823f938127e
hira <devilthemes@gmail.com>
2025-06-17 22:08:24 +0545

    fixing issues

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Optional: enables SWC-based minification and other performance options
-  swcMinify: true,
 };
 
 module.exports = nextConfig;
